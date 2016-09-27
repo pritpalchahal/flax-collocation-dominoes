88ef280abba87990b7df6328d6a34a6cafa76c0b
prit <prit.chahal@gmail.com>
2016-09-27 17:54:01 +1300

    removed Keyboard plugin as not required

--- www/js/app.js
+++ www/js/app.js
@@ -52,19 +52,6 @@ angular.module('collocationdominoes', ['ionic', 'collocationdominoes.controllers
   var isEdge = ionic.Platform.isEdge();//works for windows 10 phones
 
   $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if(!isEdge){
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
