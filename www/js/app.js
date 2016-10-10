6867aad178c40c7b20325c32b5af6fd57cf2ba91
prit <prit.chahal@gmail.com>
2016-10-11 02:00:06 +1300

    Fix bug

--- www/js/app.js
+++ www/js/app.js
@@ -96,7 +96,7 @@ angular.module('collocationdominoes', ['ionic', 'collocationdominoes.controllers
         SummaryData.updateEndTime(collId,exId,timeNow);
       }
 
-      if(SummaryData.getSummary(collId,exId).score == words.length){
+      if(words && SummaryData.getSummary(collId,exId).score == words.length){
         StateData.updateState(collId,exId,"Complete");
       }
       else{
